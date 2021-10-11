0534452da245890cfabfb3511520dcf6ecc17310
Gunarto Septiawan <gunartosept@gmail.com>
2021-10-11 15:59:56 +0700

    tugas_15_javascript
    Tugas Niomic

---
+++ tugas15.js
@@ -0,0 +1,14 @@
+var ab = 8;
+var bc = 6;
+
+console.log("Nilai AB : ");
+console.log(ab);
+console.log("Nilai BC : ");
+console.log(bc);
+console.log("Panjang sisi AC pada segitiga siku-siku tersebut adalah : ");
+
+function pythagoras(){
+  console.log(Math.sqrt(Math.pow(ab,2) + Math.pow(bc,2)));
+}
+pythagoras();
+
